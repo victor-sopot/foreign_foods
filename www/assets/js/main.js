e1614cb02c1073fb1c22c38be6fde7aca2342b09
Adam Sofokleous <adamcyp@gmail.com>
2015-12-30 13:36:19 +0000

    Fixed media query issue
    Added media query for tablet to match bootstraps breakpoints. comments
    in the jquery

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -40,19 +40,38 @@ $(document).ready(function(){
 
 	function checkuser() {
 		if(hoodie.account.username){
+
+			//Hide the form after login/register
 			$("#loginPage").hide();
 			$("#signupPage").hide();
+
+			//Show confirmation on login or signup
 			$("#userConf").show();
 			$("#userConf h2").text("Welcome, " + hoodie.account.username + "! Redirecting...");
+
+			//Update navbar text
 			$("#currentUser").text("Signed In As " + hoodie.account.username);
+
+			//Hide Register+Login buttons, show logout button if logged in
+			$("#register").hide();
 			$("#login").hide();
 			$("#logout").show();
+
 		} else {
+			
+			//Show the form by default (not logged in)
 			$("#loginPage").show();
 			$("#signupPage").show();
+
+			//Hide confirmation messages
 			$("#userConf").hide();
 			$("#userConf h2").text("");
+
+			//Navbar text
 			$("#currentUser").text("Not Signed In");
+
+			//Show Register+Login buttons if not signed in, hide logout button
+			$("#register").show();
 			$("#login").show();
 			$("#logout").hide();
 		}
