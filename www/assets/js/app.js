c9943c88190e294dcd3a26dd7ffee814f399ad7e
Adam Sofokleous <adamcyp@gmail.com>
2016-08-17 02:06:30 +0100

    Testing

---
+++ www/assets/js/app.js
@@ -0,0 +1,54 @@
+// /* AngularJS App 
+// Adam Sofokleous */
+// 'use strict';
+
+// var app = angular.module('foreign', [
+// 	'ngRoute', 'ngAnimate'
+// ]);
+
+// /* Routes */
+// // app.config(['$routeProvider', function($routeProvider){
+// // 	$routeProvider
+// // 	//Home
+// // 	.when("/", {templateUrl: "index.html", controller: "CheckLogin"})
+// // 	.otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+// // }]);
+
+// app.controller('CheckLogin', function($scope){
+// 	console.log('Hello World');
+// 	// if (hoodie.account.username)
+// 	// {
+// 	// 	$scope.response = 'success';
+// 	// }
+
+// });
+
+// // app.directive('menuBar', function() {
+// // 	return {
+// // 		// scope:
+// // 		restrict: 'AE',
+// // 		replace: 'true',
+// // 		templateUrl: 'templates/menu_bar.html'
+// // 	}
+// // });
+
+// // app.directive('pageHeader', function() {
+// // 	return {
+// // 		restrict: 'AE',
+// // 		replace: 'true',
+// // 		templateUrl: 'templates/page_header.html'
+// // 	}
+// // });
+
+// // app.directive("login", function () {
+// // 	return {
+// // 		restrict: "E",
+// // 		replace: true,
+// // 		templateUrl: "login.html",
+// // 		link: function (scope, element) {
+// // 			scope.$watch("model.input", function(value){
+// // 				hoodie.account.signIn({})
+// // 			})
+// // 		}
+// // 	}
+// // })
