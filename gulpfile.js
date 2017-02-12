2c4ea87c40bac8da30eeed1fe07e5969bcb933b3
Adam Sofokleous <adamcyp@gmail.com>
2017-02-12 15:55:30 +0000

    - Init bower - Set up gulp task to bundle bower dependencies into a minified js file - Added script links in index appropriately

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var browserSync = require('browser-sync'),
+	gulp = require('gulp'),
+	changed = require('gulp-changed'),
+	jscs = require('gulp-jscs'),
+	notify = require('gulp-notify'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat');
+
+var config = {
+	bowerDir: './bower_components/',
+	dest: './www/assets/js/'
+}
+
+
+gulp.task('default', ['bundle-scripts'], function() {
+  // place code for your default task here
+});
+
+gulp.task('bower', function() {
+	return bower()
+		.pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('bundle-scripts', function() {
+	var bower = [
+		config.bowerDir + 'jquery/dist/jquery.js',
+		config.bowerDir + 'underscore/underscore.js',
+		config.bowerDir + 'backbone/backbone.js',
+		config.bowerDir + 'bootstrap/dist/js/bootstrap.js',
+		config.bowerDir + 'jquery-easing/jquery.easing.js'
+	]
+	return gulp.src(bower)
+		.pipe(concat('bundle.js'))
+		.pipe(uglify())
+		.pipe(rename({ extname: '.min.js' }))
+		.pipe(gulp.dest('./www/assets/js/'))
+});
